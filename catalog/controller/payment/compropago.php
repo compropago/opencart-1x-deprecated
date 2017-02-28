5dd0e24a902e7b706f1ab2b368088e978beb2bdd
Jesus Alfredo <alfredo@compropago.com>
2017-02-28 10:58:44 -0600

    fix issue

--- catalog/controller/payment/compropago.php
+++ catalog/controller/payment/compropago.php
@@ -182,7 +182,7 @@ class ControllerPaymentCompropago extends Controller
                         $this->config->get('compropago_order_status_approve_id'),
                         $comment
                     );
-                    $this->db->query("UPDATE " . DB_PREFIX . "`order` SET order_status_id = '" . (int)$this->config->get('compropago_order_status_approve_id') . "' WHERE order_id = '" . (int)$response->order_info->order_id . "'");
+                    $this->db->query("UPDATE `" . DB_PREFIX . "order` SET order_status_id = '" . (int)$this->config->get('compropago_order_status_approve_id') . "' WHERE order_id = '" . (int)$response->order_info->order_id . "'");
                     printf("[%s]\n",      $comment);
                     break;
                 case 'charge.pending':
@@ -193,7 +193,7 @@ class ControllerPaymentCompropago extends Controller
                         $comment
                     );
                     
-                    $this->db->query("UPDATE " . DB_PREFIX . "`order` SET order_status_id = '" . (int)$this->config->get('compropago_order_status_new_id') . "' WHERE order_id = '" . (int)$response->order_info->order_id . "'" );
+                    $this->db->query("UPDATE `" . DB_PREFIX . "order` SET order_status_id = '" . (int)$this->config->get('compropago_order_status_new_id') . "' WHERE order_id = '" . (int)$response->order_info->order_id . "'" );
                     printf("[%s]\n",      $comment);
 
 
